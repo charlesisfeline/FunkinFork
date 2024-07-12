c7cdd6f91594b84c23f1d44ff1f7332aca5f9d40
gamerbross <55158797+gamerbross@users.noreply.github.com>
2024-07-12 08:05:42 +0200

    Remove unnecessary player vocals muting

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -2676,7 +2676,6 @@ class PlayState extends MusicBeatSubState
           });
       }
     }
-    vocals.playerVolume = 0;
 
     applyScore(-10, 'miss', healthChange, true);
 
@@ -2737,7 +2736,6 @@ class PlayState extends MusicBeatSubState
 
     if (event.playSound)
     {
-      vocals.playerVolume = 0;
       FunkinSound.playOnce(Paths.soundRandom('missnote', 1, 3), FlxG.random.float(0.1, 0.2));
     }
   }
