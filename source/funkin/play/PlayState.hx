7395024546f1f4f73dbb95e386bbf2517eb4c021
Lasercar <B_l_u_t_a_c_k@hotmail.com.au>
2025-02-08 20:39:13 +1000

    fixed success message (also forgor playstate change)

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -2780,6 +2780,8 @@ class PlayState extends MusicBeatSubState
         FlxG.switchState(() -> new ChartEditorState(
           {
             targetSongId: currentSong.id,
+            targetSongDifficulty: currentDifficulty,
+            targetSongVariation: currentVariation,
           }));
       }
     }
