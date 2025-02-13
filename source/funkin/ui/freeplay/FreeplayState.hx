29356a8d867c96b7dde5ef920730e581b7f8f981
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-02-12 20:17:15 -0500

    Squashed commit of the following:
    commit 9657c77e1f18f72cb28a86df2ef140c89f1f1e32
    Author: Lasercar <B_l_u_t_a_c_k@hotmail.com.au>
    Date:   Fri Feb 7 20:33:20 2025 +1000
    
        idk
    
    commit b888d1f3aca6e4674eb8b7210789d6152ad84528
    Author: Lasercar <B_l_u_t_a_c_k@hotmail.com.au>
    Date:   Fri Feb 7 20:29:23 2025 +1000
    
        Open chart editor in freeplay

--- source/funkin/ui/freeplay/FreeplayState.hx
+++ source/funkin/ui/freeplay/FreeplayState.hx
@@ -54,6 +54,7 @@ import funkin.util.SortUtil;
 import openfl.display.BlendMode;
 import funkin.data.freeplay.style.FreeplayStyleRegistry;
 import funkin.data.song.SongData.SongMusicData;
+import funkin.ui.debug.charting.ChartEditorState;
 #if FEATURE_DISCORD_RPC
 import funkin.api.discord.DiscordClient;
 #end
@@ -1707,6 +1708,19 @@ class FreeplayState extends MusicBeatSubState
     {
       busy = true;
       moveToResultsScreen();
+    }
+
+    if (controls.DEBUG_CHART && !busy)
+    {
+      /*
+        Doing it this way rather than passing the rememberedSongId
+        so that in the future, this can be made to load a random song when given this
+       */
+      var targetSong = grpCapsules.members[curSelected]?.freeplayData?.data.id ?? 'unknown';
+      FlxG.switchState(() -> new ChartEditorState(
+        {
+          targetSongId: targetSong,
+        }));
     }
   }
 
