afdc7a53e4b27df900f1f4cec38b1b59f6a538e9
Lasercar <B_l_u_t_a_c_k@hotmail.com.au>
2025-02-15 16:07:55 +1000

    removed commented out line

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -2178,8 +2178,6 @@ class PlayState extends MusicBeatSubState
      */
   function onKeyRelease(event:PreciseInputEvent):Void
   {
-    // if (isGamePaused) return;
-
     // Do the minimal possible work here.
     inputReleaseQueue.push(event);
   }
