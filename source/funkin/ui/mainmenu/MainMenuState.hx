ecfbf41fe758d8ba781c11aa15dde937907c8f89
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-03-07 16:28:30 -0500

    Revert "Cancel state change on debug menu"
    This reverts commit b5cd572aea6d2653230a47d265b5b8c1d76e5b2b.

--- source/funkin/ui/mainmenu/MainMenuState.hx
+++ source/funkin/ui/mainmenu/MainMenuState.hx
@@ -196,7 +196,6 @@ class MainMenuState extends MusicBeatState
     FlxG.camera.follow(camFollow, null, 0.06);
 
     if (snap) FlxG.camera.snapToTarget();
-    if (menuItems.busy) menuItems.busy = false;
   }
 
   function createMenuItem(name:String, atlas:String, callback:Void->Void, fireInstantly:Bool = false):Void
@@ -355,9 +354,6 @@ class MainMenuState extends MusicBeatState
     if (controls.DEBUG_MENU)
     {
       persistentUpdate = false;
-
-      // Cancel the currently flickering menu item because it's about to call a state switch
-      if (menuItems.busy) FlxFlicker.stopFlickering(menuItems.members[menuItems.selectedIndex]);
 
       FlxG.state.openSubState(new DebugMenuSubState());
     }
