48c1669e860287746c660c62c6669c657bfbeec9
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-03-19 16:00:21 +1000

    Character icon clickable in playstate fix

--- source/funkin/ui/debug/charting/ChartEditorState.hx
+++ source/funkin/ui/debug/charting/ChartEditorState.hx
@@ -2844,18 +2844,19 @@ class ChartEditorState extends UIState // UIState derives from MusicBeatState
 
     // Setup character dropdowns.
     FlxMouseEvent.add(healthIconDad, function(_) {
-      if (!isCursorOverHaxeUI)
+      if (!isCursorOverHaxeUI && persistentDraw && persistentUpdate)
       {
         this.openCharacterDropdown(CharacterType.DAD, true);
       }
     });
 
     FlxMouseEvent.add(healthIconBF, function(_) {
-      if (!isCursorOverHaxeUI)
+      if (!isCursorOverHaxeUI && persistentDraw && persistentUpdate)
       {
         this.openCharacterDropdown(CharacterType.BF, true);
       }
     });
+
 
     buttonSelectOpponent = new Button();
     buttonSelectOpponent.allowFocus = false;
