8ee6d8cf21c77c6f3eca2ad4f9cb6019b08ddfff
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2025-03-27 00:32:56 -0400

    window color cool

--- source/funkin/ui/transition/preload/FunkinPreloader.hx
+++ source/funkin/ui/transition/preload/FunkinPreloader.hx
@@ -131,11 +131,6 @@ class FunkinPreloader extends FlxBasePreloader
 
   public function new()
   {
-    #if (cpp && windows)
-    // Enable dark mode support for the title bar.
-    funkin.util.WindowUtil.setWindowDarkMode(true, true);
-    #end
-
     super(Constants.PRELOADER_MIN_STAGE_TIME);
 
     // We can't even call trace() yet, until Flixel loads.
