37be5181e288fbfeed10e6eccc726d7b99ffaca8
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2025-03-28 13:39:37 -0300

    fix: Chart loading fixes
    * Prevents charter from deleting `default` variation when there's more than one variation available.
    * Fixes song variation list not being saved into `default` metadata file.
    * Fixes loading from FNFC incorrectly loading all variations as `default`.
    * Fixes loading from FNFC incorrectly using the `default` variation metadata for every other variation.

--- source/funkin/ui/debug/charting/handlers/ChartEditorImportExportHandler.hx
+++ source/funkin/ui/debug/charting/handlers/ChartEditorImportExportHandler.hx
@@ -253,7 +253,8 @@ class ChartEditorImportExportHandler
       var variMetadataVersion:Null<thx.semver.Version> = VersionUtil.getVersionFromJSON(variMetadataString);
       if (variMetadataVersion == null) throw 'Could not read metadata ($variation) version.';
 
-      var variMetadata:Null<SongMetadata> = SongRegistry.instance.parseEntryMetadataRawWithMigration(baseMetadataString, variMetadataPath, variMetadataVersion);
+      var variMetadata:Null<SongMetadata> = SongRegistry.instance.parseEntryMetadataRawWithMigration(variMetadataString, variMetadataPath,
+        variMetadataVersion, variation);
       if (variMetadata == null) throw 'Could not read metadata ($variation).';
       songMetadatas.set(variation, variMetadata);
 
@@ -264,7 +265,7 @@ class ChartEditorImportExportHandler
       if (variChartDataVersion == null) throw 'Could not read chart data version ($variation).';
 
       var variChartData:Null<SongChartData> = SongRegistry.instance.parseEntryChartDataRawWithMigration(variChartDataString, variChartDataPath,
-        variChartDataVersion);
+        variChartDataVersion, variation);
       if (variChartData == null) throw 'Could not read chart data ($variation).';
       songChartDatas.set(variation, variChartData);
     }
